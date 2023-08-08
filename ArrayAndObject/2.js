2322f6628cc3538b19d1789f9149012163d8fd93
Andrei Kramski <Andrei_Kramski@epam.com>
2023-08-08 17:31:51 +0200

    3 коммент

--- ArrayAndObject/2.js
+++ ArrayAndObject/2.js
@@ -29,21 +29,34 @@ const resultElement = document.getElementById('list');
 //5 теперь нам нужно сделать чтобы у каждого значения в списке было два статуса выполнен или не выполне. мы можем преобразовать наш массив выше в:
 const notes = [
   { title: 'theory', completed: false },
-  { title: 'Array', completed: true },
+  { title: 'Array', completed: false },
 ];
 //2.1 создаем новую функцию которая будет это делать
 function render() {
   //4 теперь чтобы автоматизировать работу с массивом нужно добавить в код ниже цикл for
-  /*
-  for (let i = 0; i < notes.length; i++) {
-    resultElement.insertAdjacentHTML('beforeend', getNoteTemplates(notes[i]));
+
+  // 10.1.2 чтобы выводить корректно все элементы и данные в шаблоне обновлялись нужно очищать список, для этого добавим перед циклом for
+  resultElement.innerHTML = '';
+  //Конец. добавим текст Нет элементов если нах массив пуст
+  if (notes.length === 0) {
+    resultElement.innerHTML = '<p>Нет элементов</p>';
   }
-  */
+
+  for (let i = 0; i < notes.length; i++) {
+    //8 чтобы получить индекс нажимаемого на странице объекта используем цикл for изменив его под эту задачу и добавив i(index) в getNoteTemplates(notes[i], ...)
+
+    resultElement.insertAdjacentHTML(
+      'beforeend',
+      getNoteTemplates(notes[i], i)
+    );
+  }
+
   //4.1 !!! можно упростить цикл и использовать более современный for of !!!
+  /*
   for (let note of notes) {
     resultElement.insertAdjacentHTML('beforeend', getNoteTemplates(note));
   }
-
+*/
   //4.2 комментим весь код ниже т.к. он перебрался в цикл выше
   /*  resultElement.insertAdjacentHTML(
     // для вывода каждого элемента массива и значений при вводе мы используем один и тот же код, поэтому закомментированный ниже код с HTML выносим в отдельную функцию 3 и переиспользуем далее по коду. Это поможет содержать код в порядке и легко обслуживать
@@ -76,13 +89,16 @@ BtnElement.onclick = function () {
     completed: false,
   };
 
+  //10 при добавления индекса к кнопкам возникает проблема с навыми значениями, тк они еще не определены система возвращает их индекс как undefined тк массив const notes = [{ title: 'theory', completed: false },{ title: 'Array', completed: true },]; не связан с переменной newNote. Для этого необходимо убрать код resultElement.insertAdjacentHTML:
+
+  /*
   resultElement.insertAdjacentHTML(
     // с помощью .insertAdjacentHTML мы можем работать с HTML ниже по коду. 'beforeend' указывает сценарий работы кода
     'beforeend',
     //мы можем закомментить HTML и вмсето него использовать функцию 3 getNoteTemplates(title)
     //5.3
     getNoteTemplates(newNote) // теперь после 5.2 вместо getNoteTemplates(interElement.value) у нас будет getNoteTemplates(newNote)
-    /*   `<li
+       `<li
   class="list-group-item d-flex justify-content-between align-items-center"
 >
   <span>${interElement.value}</span>
@@ -91,12 +107,34 @@ BtnElement.onclick = function () {
     <span class="btn btn-small btn-danger">&times;</span>
   </span>
   </li>`
-  */
+  
   );
+  */
+  //10.1 и добавим сюда наш массив используя метод push который добав элементы в конец массива
+  notes.push(newNote);
+  render(); //10.1.1 чтобы новые элемнты массива отображались нужно добавить вызов метода render
   interElement.value = '';
+
+  // 11 добавим обработчик события клика на resultElement
+  resultElement.onclick = function (event) {
+    // данная функция всегда принимает параметр события event это object который всегда возвращ кликнутый объект на странице
+    // 12
+    if (event.target.dataset.index) {
+      // создадим пременную распарсим ее в цифры и с ее помощью пройтя в объект мы получим значение index если оно там есть, так же и для type только без распарсить в цифры
+      const index = parseInt(event.target.dataset.index);
+      const type = event.target.dataset.type;
+      //13 теперь реализуем логику нажатия кнопок и в зависимости от type будем совершать действие. тк это булевое значение мы можем обратиться к массиву notes и в зависимости от типа изменить его значение на противоположное используя !
+      if (type === 'toggle') {
+        notes[index].completed = !notes[index].completed;
+      } else if (type === 'remove') {
+        notes.splice(index, 1); //  для удаления используем метод splice указав что именно удаляем и сколько через (index, 1)
+      }
+      render(); // чтобы новые элемнты массива отображались нужно добавить вызов метода render
+    }
+  };
 };
 //3 создаем функцию которая облегчит работу с котом и позволит переиспольз HTML шаблон
-function getNoteTemplates(note) {
+function getNoteTemplates(note, index) {
   //5.1 т.к. мы начали работать с объекктами нам нужно показать что выводить мы теперь будем значения этих объектов поэтому аргумент title меняем на note из 4.1. тк это объект мы через точку можем обратиться к его свойству title в 5
   // задаем функции наш аргумент title который будет передаваться в HTML при работе с этой функцией далее в коде
   return `<li
@@ -106,12 +144,16 @@ function getNoteTemplates(note) {
             note.completed ? 'text-decoration-line-through' : ''
           }">${note.title}</span>
           <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            <span class="btn btn-small btn-${
+              note.completed ? 'warning' : 'success'
+            } "data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
           </span>
           </li>`;
-} // 6 добавим зачеркивание для статуса true значения на 105 строке используя тернарный оператор
-
+} // 6 добавим зачеркивание для статуса true значения span class="${note.completed ?  строке используя тернарный оператор
+// 7 добавим изменеия цвета при смене статуса заметки <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}
+//9 теперь нужно добавить index к нашим кнопкам, чтобы дальше определять какая была нажата
+// добавим ещё дата тип  data-type="remove" в "data-index="${index}" data-type="toggle">&check;</span>           <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
 // Object theory
 /*
 const person = {
